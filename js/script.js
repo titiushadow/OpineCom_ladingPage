8901be02df2c05b0fdefcb97275572277d4270cb
titiushadow <mesaque1212@gmail.com>
2023-04-24 09:01:08 -0300

    Quase finalizando as avaliações

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Navbar responsiva no mobile
 var ul = document.querySelector('nav ul');
 var menuBtn = document.querySelector('.menu-btn i');
 
@@ -9,22 +10,42 @@ function menuShow() {
     }
 }
 
+// Mostrar divs melhores empresas e piores empresas no mobile
 const mostrarPioresEmpresasBtn = document.getElementById("mostrar-piores-empresas");
 const mostrarMelhoresEmpresasBtn = document.getElementById("mostrar-melhores-empresas");
 const rankingGoodDiv = document.querySelector(".ranking-good");
 const rankingBadDiv = document.querySelector(".ranking-bad");
 
 mostrarPioresEmpresasBtn.addEventListener("click", function() {
-    console.log("Clicou em piores empresas");
     rankingGoodDiv.classList.add("hide");
     rankingBadDiv.classList.remove("hide");
 });
 
 mostrarMelhoresEmpresasBtn.addEventListener("click", function() {
-    console.log("Clicou em melhores empresas");
     rankingBadDiv.classList.add("hide");
     rankingGoodDiv.classList.remove("hide");
 });
+
+// Carrosel de avaliações
+const backButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+const cardsAvali = document.querySelector(".row-avaliacoes div");
+const cards = document.querySelectorAll(".cards-avaliacoes");
+let currentCardIndex = 0;
+
+backButton.addEventListener("click", function() {
+    if (currentCardIndex > 0) {
+        currentCardIndex--;
+        cardsAvali.style.transform = `translateX(-${currentCardIndex * 100}%)`;
+        cardsAvali.style.transition = "transform 0.8s ease-in-out";
+    }
+});
+
+nextButton.addEventListener("click", function() {
+
+})
+
+
 
 
 
